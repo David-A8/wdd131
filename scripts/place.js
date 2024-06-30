066cf3d0ab88c287433b82ace5009acc74b14b6b
David-A8 <davidamadorc@gmail.com>
2024-06-29 22:15:20 -0600

    Week 3 Assignment

---
+++ scripts/place.js
@@ -0,0 +1,8 @@
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+let oLastModif = new Date(document.lastModified)
+
+class Weather {
+    static temperature = 80;
+    static wind = 5;
+}
+
