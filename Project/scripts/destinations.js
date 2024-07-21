f6293ed4b40372d698e3e1391646f7b3e06de752
David-A8 <davidamadorc@gmail.com>
2024-07-21 10:47:05 -0600

    Form html added

--- Project/scripts/destinations.js
+++ Project/scripts/destinations.js
@@ -2,6 +2,14 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 let oLastModif = new Date(document.lastModified)
 document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleDateString();
 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
 const destinations = [
     {
         destination: "San Francisco",
